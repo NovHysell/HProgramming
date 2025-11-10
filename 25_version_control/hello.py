f91d381843197ad9a9782a47fdbb1b74d98405e1
NovHysell <bhysell8058@floridapoly.edu>
2025-11-10 16:29:28 +0000

    Create hello.py

---
+++ 25_version_control/hello.py
@@ -0,0 +1,11 @@
+# Git is a version control system
+# Tracks changes in code over time, lets you collaborate with others safely
+# "Save As" + "Undo" on steroids
+
+# git init - initializes a new git repository
+# git status - shows status of changes
+# git log - shows commit history
+# git log --online
+# git diff <file> - shows unstaged changes
+# git restore <file> - discards changes for a file
+print("Hello World!")
